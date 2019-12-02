7252bc9c6da6fc9f33ef65df2f49b0c553141c6d
Przemysław Wojnowski <esperanto@cumego.com>
2019-12-02 14:41:14 +0100

    Update Clojure version and add aoc2019.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Solutions to Advent Of Code Puzzles"
   :url "http://farenda.com/clojure"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns aoc.aoc2018})
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns aoc.aoc2019})
