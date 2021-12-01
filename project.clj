ddc76d22a9bcc508db3ee48a10ee071cd708f511
Przemysław Wojnowski <esperanto@cumego.com>
2021-12-01 09:13:12 +0100

    Upgrade Clojure to 1.10.3

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "Solutions to Advent Of Code Puzzles"
   :url "http://farenda.com/clojure"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :main aoc.core
   :repl-options {:init-ns aoc.aoc2020})
