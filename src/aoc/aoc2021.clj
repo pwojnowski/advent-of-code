bbb6504450389b55bf2bb6780ddcde4162339046
Przemysław Wojnowski <esperanto@cumego.com>
2021-12-03 20:07:31 +0100

    Solution to day 1 part 2.

--- src/aoc/aoc2021.clj
+++ src/aoc/aoc2021.clj
@@ -11,3 +11,17 @@
         cnt
         (recur (first xs) (rest xs) (inc i)
                (if (< prev (first xs)) (inc cnt) cnt))))))
+
+(defn day-01-2 [input]
+  (let [numbers (read-numbers input)
+        len (count numbers)]
+    (loop [psum (reduce '+ (take 3 numbers))
+           i 3
+           cnt 0]
+      (if (= i len)
+        cnt
+        (let [sum (+ (- psum (get numbers (- i 3)))
+                     (get numbers i))]
+          (recur sum
+                 (inc i)
+                 (if (< psum sum) (inc cnt) cnt)))))))
--- src/aoc/utils.clj
+++ src/aoc/utils.clj
@@ -1,12 +1,12 @@
 (ns aoc.utils
   (:require [clojure.string :as s]))
 
-(defn load-file [filepath]
+(defn read-file [filepath]
   (let [abs-path (s/replace-first filepath "~" (System/getProperty "user.home"))]
     (slurp abs-path)))
 
-(defn load-lines [filepath]
-  (s/split-lines (load-file filepath)))
+(defn read-lines [filepath]
+  (s/split-lines (read-file filepath)))
 
 (defn read-numbers
   "Parse `input' into a vector of numbers."
