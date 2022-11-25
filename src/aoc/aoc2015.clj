9d74cb35f13cb06b4b1d1c1bde0f920424697502
Przemysław Wojnowski <esperanto@cumego.com>
2022-11-25 16:26:30 +0100

    Solution to 2015-02-1

--- src/aoc/aoc2015.clj
+++ src/aoc/aoc2015.clj
@@ -11,3 +11,14 @@
              (+ level (if (= c \() 1 -1))
              (first chars)
              (rest chars)))))
+
+(defn- day-02-calculate-required-paper [box-sizes]
+  (let [[l w h] (map parse-long (clojure.string/split box-sizes #"x"))
+        lw (* l w)
+        lh (* l h)
+        wh (* w h)
+        smallest-side (min lw lh wh)]
+    (+ (* 2 (+ lw lh wh)) smallest-side)))
+
+(defn day-02-part-1 [input]
+  (reduce + 0 (map day-02-calculate-required-paper input)))
