8dbcfc0d09cb8d274e7c6c36a9a013e6684e694b
Przemysław Wojnowski <esperanto@cumego.com>
2022-11-25 16:53:09 +0100

    Solution to 2015-02-2

--- src/aoc/aoc2015.clj
+++ src/aoc/aoc2015.clj
@@ -12,8 +12,11 @@
              (first chars)
              (rest chars)))))
 
-(defn- day-02-calculate-required-paper [box-sizes]
-  (let [[l w h] (map parse-long (clojure.string/split box-sizes #"x"))
+(defn- day-02-get-box-sizes [s]
+  (map parse-long (clojure.string/split s #"x")))
+
+(defn- day-02-calculate-required-paper [s]
+  (let [[l w h] (day-02-get-box-sizes s)
         lw (* l w)
         lh (* l h)
         wh (* w h)
@@ -22,3 +25,13 @@
 
 (defn day-02-part-1 [input]
   (reduce + 0 (map day-02-calculate-required-paper input)))
+
+(defn- day-02-calculate-required-ribbon [s]
+  (let [[l w h] (day-02-get-box-sizes s)
+        biggest (* 2 (max l w h))
+        perimeter (- (* 2 (+ l w h)) biggest)
+        bow-ribbon (* l w h)]
+    (+ perimeter bow-ribbon)))
+
+(defn day-02-part-2 [input]
+  (reduce + 0 (map day-02-calculate-required-ribbon input)))
