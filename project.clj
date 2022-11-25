ccaa10b2dff1c1fa9130c90ae35c93f90c729b7f
Przemysław Wojnowski <esperanto@cumego.com>
2022-11-25 09:29:05 +0100

    Update Clojure to 1.11.1 and add ns for aoc2022

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject aoc "0.1.0-SNAPSHOT"
   :description "Solutions to Advent Of Code Puzzles"
-  :url "http://farenda.com/clojure"
+  :url "https://farenda.com/clojure"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :main aoc.core
   :repl-options {:init-ns aoc.aoc2021})
---
+++ src/aoc/aoc2022.clj
@@ -0,0 +1,3 @@
+(ns aoc.aoc2022
+  (:require [clojure.string :as cstr]
+            [aoc.utils :refer [read-numbers]]))
