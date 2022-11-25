442802d8c1fe022d8c949a6e3326cd4485dd6444
Przemysław Wojnowski <esperanto@cumego.com>
2022-11-25 09:48:27 +0100

    Solution to 2015-01-2

--- src/aoc/aoc2015.clj
+++ src/aoc/aoc2015.clj
@@ -2,3 +2,12 @@
 
 (defn day-01-what-floor [input]
   (reduce #(+ % (if (= %2 \() 1 -1)) 0 input))
+
+(defn day-01-part-2 [input]
+  (loop [i 0 level 0 c (first input) chars (rest input)]
+    (if (= level -1)
+      i
+      (recur (inc i)
+             (+ level (if (= c \() 1 -1))
+             (first chars)
+             (rest chars)))))
