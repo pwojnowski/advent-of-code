06e72770c6c4350ab227859d500651a21d6e56b0
Przemysław Wojnowski <esperanto@cumego.com>
2022-12-02 08:10:18 +0100

    Reformat input

--- src/aoc/aoc2022.clj
+++ src/aoc/aoc2022.clj
@@ -13,15 +13,9 @@
 (def day-02-1-choice-scores {\X 1 \Y 2 \Z 3})
 
 (def day-02-1-score-table
-  {"A X" 3
-   "A Y" 6
-   "A Z" 0
-   "B X" 0
-   "B Y" 3
-   "B Z" 6
-   "C X" 6
-   "C Y" 0
-   "C Z" 3})
+  {"A X" 3, "A Y" 6, "A Z" 0,
+   "B X" 0, "B Y" 3, "B Z" 6,
+   "C X" 6, "C Y" 0, "C Z" 3})
 
 (defn- day-02-1-score-round [s]
   (+ (day-02-1-choice-scores (last s))
@@ -33,15 +27,9 @@
 (def day-02-2-score-result {\X 0 \Y 3 \Z 6})
 
 (def day-02-2-choice-table
-  {"A X" 3
-   "A Y" 1
-   "A Z" 2
-   "B X" 1
-   "B Y" 2
-   "B Z" 3
-   "C X" 2
-   "C Y" 3
-   "C Z" 1})
+  {"A X" 3, "A Y" 1, "A Z" 2,
+   "B X" 1, "B Y" 2, "B Z" 3
+   "C X" 2, "C Y" 3, "C Z" 1})
 
 (defn- day-02-2-score-round [s]
   (+ (day-02-2-score-result (last s))
