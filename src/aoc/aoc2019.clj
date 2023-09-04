6b47138ea3eabf622c663052f3e0f980bdfaead6
Przemysław Wojnowski <esperanto@cumego.com>
2023-09-04 12:16:13 +0200

    Solution to 2019-01-part2

--- src/aoc/aoc2019.clj
+++ src/aoc/aoc2019.clj
@@ -7,3 +7,16 @@
   (->> masses
        (map day01-calculate-fuel)
        (reduce + 0)))
+
+(defn- day01-calculate-non-negative-fuel [mass]
+  (let [fuel (day01-calculate-fuel mass)]
+    (max 0 fuel)))
+
+(defn- day01-calculate-total-fuel [mass]
+  (->> (day01-calculate-non-negative-fuel mass)
+       (iterate day01-calculate-non-negative-fuel)
+       (take-while pos?)
+       (apply +)))
+
+(defn day01-part2 [masses]
+  (reduce + 0 (map day01-calculate-total-fuel masses)))
--- test/aoc/aoc2019_test.clj
+++ test/aoc/aoc2019_test.clj
@@ -6,3 +6,10 @@
   (let [masses [12 14 1969 100756]
         expected-fuel (+ 2 2 654 33583)]
     (is (= expected-fuel (aoc/day01-part1 masses)))))
+
+(deftest test-day-01-part2
+  (testing "Fuel for single mass"
+    (is (= 2 (aoc/day01-part2 [12])))
+    (is (= 2 (aoc/day01-part2 [14])))
+    (is (= 966 (aoc/day01-part2 [1969])))
+    (is (= 50346 (aoc/day01-part2 [100756])))))
